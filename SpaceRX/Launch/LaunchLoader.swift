c581d197d8e48001a080682897390dbc05dfb8d8
GregoryDushin <86513789+GregoryDushin@users.noreply.github.com>
2023-05-30 17:35:09 +0200

    swL

--- SpaceRX/Launch/LaunchLoader.swift
+++ SpaceRX/Launch/LaunchLoader.swift
@@ -12,10 +12,10 @@ import RxSwift
 // MARK: - JSON lAUNCH PARSING
 
 final class LaunchLoader {
-    
+
     private let decoder = JSONDecoder()
     private let session: URLSession
-    
+
     init(urlSession: URLSession = .shared) {
         let dateFormatter = DateFormatter()
         decoder.dateDecodingStrategy = .formatted(dateFormatter)
@@ -23,12 +23,12 @@ final class LaunchLoader {
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         self.session = urlSession
     }
-    
+
     func loadLaunchData(id: String) -> Single<[LaunchModelElement]> {
         guard let url = URL(string: Url.launchUrl) else {
             return .error(NSError(domain: "InvalidURL", code: 0, userInfo: nil))
         }
-        
+
         let request = URLRequest(url: url)
         return session.rx.data(request: request)
             .map { data -> [LaunchModelElement] in
