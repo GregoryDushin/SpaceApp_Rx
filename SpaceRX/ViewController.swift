09b956910bc317650ee815da68549099f7484c01
GregoryDushin <86513789+GregoryDushin@users.noreply.github.com>
2023-05-30 13:30:36 +0200

    Initial Commit

---
+++ SpaceRX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceRX
+//
+//  Created by Григорий Душин on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
